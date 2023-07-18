a5318f10def015748673e5736d15cc060deba3ad
usharani2022 <99280045+usharani2022@users.noreply.github.com>
2023-07-18 17:35:37 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -6,9 +6,9 @@ def newBuild()
 {
    sh 'mvn package'
 }
-def newDeploy(jobname,ipadress,content)
+def newDeploy(jobname,ipadress,context)
 {
-   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/{content}.war cameluser@{$ipadress}:/var/lib/tomcat9/webapps/{content}.war"
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war cameluser@{$ipadress}:/var/lib/tomcat9/webapps/{context}.war"
 }
 
 
